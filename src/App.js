9dff9efb1e08e37e36ffc5574741f58cbf2f26fb
Usman Asif <usmanasif193@gmail.com>
2021-08-03 13:00:08 +0500

    Completed App without css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Coins from './Components/Coins';
 function App() {
 
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -13,7 +14,15 @@ function App() {
         setCoins(res.data)
       })
       .catch(error => console.log(error))
-  }, [])
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.volume);
+  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="coin-app">
@@ -22,10 +31,23 @@ function App() {
           Search a Currency
         </h1>
         <form>
-          <input type="text" className="coin-input" placeholder="Search" />
+          <input type="text" className="coin-input" placeholder="Search" onChange={handleChange} />
         </form>
       </div>
-      <Coins coins={coins} />
+      {filteredCoins.map((coin) => {
+        return (
+          <Coins
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            market_cap={coin.market_cap}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        )
+      })}
     </div>
   );
 }
--- src/Components/Coins.js
+++ src/Components/Coins.js
@@ -1,18 +1,25 @@
 import React from 'react'
 
-function Coins() {
+function Coins({ image, name, symbol, price, volume, priceChange, marketcap }) {
     return (
         <div className="coin-tracker">
             <div className="coin-row">
                 <div className="coin">
-                    <img src="" alt="crypto" />
-                    <h1>"cryptocurrency name"</h1>
-                    <p className="coin-symbol">symbol</p>
+                    <img src={image} alt="crypto" />
+                    <h1>{name}</h1>
+                    <p className="coin-symbol">{symbol}</p>
                 </div>
                 <div className="coin-data">
-                    <p className="coin-price">$10</p>
-                    <p className="coin-volume">$volume</p>
-
+                    <p className="coin-price">${price}</p>
+                    <p className="coin-volume">${volume.toLocalString()}</p>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                    ) : (
+                        <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+                    )}
+                    <p className="market-cap">
+                        Mkt Cap : ${marketcap.toLocalString}
+                    </p>
                 </div>
             </div>
         </div>
