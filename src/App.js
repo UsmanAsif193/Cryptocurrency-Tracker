075343a32392200f4fcf17e229b6fed81eee1537
Usman Asif <usmanasif193@gmail.com>
2021-08-03 14:21:27 +0500

    Loading... Loader added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Coins from './Components/Coins';
@@ -7,16 +7,19 @@ function App() {
 
   const [coins, setCoins] = useState([])
   const [search, setSearch] = useState('')
+  const [ApiCall, setApiCall] = useState()
 
-  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res => {
-      setCoins(res.data)
-    })
-    .catch(error => console.log(error))
-
+  useEffect(() => {
+    let api = axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data)
+        setApiCall(api)
+      })
+      .catch(error => console.log(error))
+  }, []);
 
   const handleChange = e => {
-    setSearch(e.target.volume);
+    setSearch(e.target.value);
   };
 
   const filteredCoins = coins.filter(coin =>
@@ -33,20 +36,23 @@ function App() {
           <input type="text" className="coin-input" placeholder="Search" onChange={handleChange} />
         </form>
       </div>
-      {filteredCoins.map((coin) => {
-        return (
-          <Coins
-            key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            volume={coin.market_cap}
-            priceChange={coin.price_change_percentage_24h}
-            market_cap={coin.total_volume}
-          />
-        )
-      })}
+      {ApiCall ? (
+        <>
+          {filteredCoins.map((coin) => {
+            return (
+              <Coins
+                key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                price={coin.current_price}
+                symbol={coin.symbol}
+                volume={coin.market_cap}
+                priceChange={coin.price_change_percentage_24h}
+                market_cap={coin.total_volume}
+              />
+            )
+          })}
+        </>) : (<h1>Loading...</h1>)}
     </div>
   );
 }
