c8ef46d0c33aee4ad994df5ab6f6713e91be748b
Usman Asif <usmanasif193@gmail.com>
2021-08-08 14:01:19 +0500

    Added new library for responsiveness
    Succeeded, Some styling required

--- src/Components/CoinsMaterial.js
+++ src/Components/CoinsMaterial.js
@@ -1,71 +1,34 @@
 import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: 'white',
-    color: 'black',
-    textTransfrom: 'bold',
-    border: 0,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: 'white',
-    },
-  },
-}))(TableRow);
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import './CoinsMaterial.css'
 
 export default function CoinsMaterial({ Coins }) {
-  const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell style={{ fontWeight: 700 }}>Name</StyledTableCell>
-            <StyledTableCell style={{ fontWeight: 700 }} align="right">Symbol</StyledTableCell>
-            <StyledTableCell style={{ fontWeight: 700 }} align="right">Price</StyledTableCell>
-            <StyledTableCell style={{ fontWeight: 700 }} align="right">Volume</StyledTableCell>
-            <StyledTableCell style={{ fontWeight: 700 }} align="right">Percentage</StyledTableCell>
-            <StyledTableCell style={{ fontWeight: 700 }} align="right">Market Cap</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Coins.map((coin) => (
-            <StyledTableRow key={coin.name}>
-              <StyledTableCell component="th" scope="row">
-                <img src={coin.image} alt="" height='30px' width='30px' />
-                {coin.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{coin.symbol.toUpperCase()}</StyledTableCell>
-              <StyledTableCell align="right">${coin.current_price}</StyledTableCell>
-              <StyledTableCell align="right">${coin.market_cap.toLocaleString()}</StyledTableCell>
-              <StyledTableCell align="right">{coin.market_cap_change_percentage_24h}%</StyledTableCell>
-              <StyledTableCell align="right">{coin.total_volume.toLocaleString()}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Table>
+      <Thead>
+        <Tr>
+          <Th style={{ fontWeight: 700, textAlign: 'left' }}>Name</Th>
+          <Th style={{ fontWeight: 700, textAlign: 'left' }}>Symbol</Th>
+          <Th style={{ fontWeight: 700, textAlign: 'left' }}>Price</Th>
+          <Th style={{ fontWeight: 700, textAlign: 'left' }}>Volume</Th>
+          <Th style={{ fontWeight: 700, textAlign: 'left' }}>Percentage</Th>
+          <Th style={{ fontWeight: 700, textAlign: 'left' }}>Market Cap</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {Coins.map((coin) => (
+          <Tr className="coin-table-row" key={coin.name}>
+            <Td className="coin-name"><img src={coin.image} alt={coin.name} height="15px" width="15px" />{coin.name}</Td>
+            <Td>{coin.symbol.toUpperCase()}</Td>
+            <Td>${coin.current_price}</Td>
+            <Td>${coin.market_cap.toLocaleString()}</Td>
+            <Td>{coin.market_cap_change_percentage_24h}%</Td>
+            <Td>{coin.total_volume.toLocaleString()}</Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
   );
 }
