857ea6c92b568483373d92ed09496e5d21e67844
Usman Asif <usmanasif193@gmail.com>
2021-08-08 14:36:59 +0500

    Responsiveness Done
    Success

--- src/Components/CoinsMaterial.js
+++ src/Components/CoinsMaterial.js
@@ -19,14 +19,18 @@ export default function CoinsMaterial({ Coins }) {
       </Thead>
       <Tbody>
         {Coins.map((coin) => (
-          <Tr className="coin-table-row" key={coin.name}>
-            <Td className="coin-name"><img src={coin.image} alt={coin.name} height="15px" width="15px" />{coin.name}</Td>
-            <Td>{coin.symbol.toUpperCase()}</Td>
-            <Td>${coin.current_price}</Td>
-            <Td>${coin.market_cap.toLocaleString()}</Td>
-            <Td>{coin.market_cap_change_percentage_24h}%</Td>
-            <Td>{coin.total_volume.toLocaleString()}</Td>
-          </Tr>
+          <>
+            <div className="coin-line"></div>
+            <Tr className="coin-table-row" key={coin.name} style={{ border: 'none' }}>
+              <Td className="coin-name-section"><img src={coin.image} alt={coin.name} height="30px" width="30px" />{coin.name}</Td>
+              <Td>{coin.symbol.toUpperCase()}</Td>
+              <Td>${coin.current_price}</Td>
+              <Td>${coin.market_cap.toLocaleString()}</Td>
+              <Td>{coin.market_cap_change_percentage_24h}%</Td>
+              <Td>{coin.total_volume.toLocaleString()}</Td>
+              <hr />
+            </Tr>
+          </>
         ))}
       </Tbody>
     </Table>
