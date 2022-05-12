b02caafe28d0238dc82616c0414dfa8b8789e8fa
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-05-13 00:20:51 +0500

    feat: mobile view fixed

--- src/Components/Coins.js
+++ src/Components/Coins.js
@@ -56,13 +56,13 @@ export default function Coins({ Coins, search }) {
               <Td>{coin.symbol.toUpperCase()}</Td>
               <Td>${coin.current_price}</Td>
               <Td>${coin.market_cap.toLocaleString()}</Td>
-              {coin.market_cap_change_percentage_24h < 0 ?
-                (<Td className="red">{coin.market_cap_change_percentage_24h.toFixed(2)}%</Td>)
+              <Td>{coin.market_cap_change_percentage_24h < 0 ?
+                (<div className="red">{coin.market_cap_change_percentage_24h.toFixed(2)}%</div>)
                 :
                 (
-                  <Td className="green">{coin.market_cap_change_percentage_24h.toFixed(2)}%</Td>
+                  <div className="green">{coin.market_cap_change_percentage_24h.toFixed(2)}%</div>
                 )
-              }
+              }</Td>
               <Td>{coin.total_volume.toLocaleString()}</Td>
             </Tr>
           ))}
